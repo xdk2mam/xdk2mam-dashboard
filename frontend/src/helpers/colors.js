eebe497176dcc328f195617d50b003090aab3da1
eduardopelitti <eduardopelitti@gmail.com>
2019-04-25 21:07:22 -0300

    Add colors helper

---
+++ frontend/src/helpers/colors.js
@@ -0,0 +1,26 @@
+const Colors = Object.freeze({
+  //Monochromatic
+  LIGHT_BLUE: '#9ED2FF', 
+  PALE_LIGHT_BLUE: '#7EA8CC',
+  DARK_BLUE: '295780',
+  BLUE: '#52AEFF',
+  PALE_DARK_BLUE: '#4F6980',
+  WHITE: '#FFFFFF',  
+  //Analog
+  LIGHT_GREEN: '#91FFF8',
+  STRONG_GREEN: '#84D4E8',
+  TURQUOISE: '#9ED2FF',
+  PALE_BLUE: '#849FE8',
+  VIOLET_BLUE: '#9192FF',
+  //Basic
+  BLACK: '#000000',
+  WHITE: '#FFFFFF',
+  //Tones
+  DARKEST_BLUE: '#283440'
+})
+
+/*
+ * Exports
+ */
+
+export default Colors
