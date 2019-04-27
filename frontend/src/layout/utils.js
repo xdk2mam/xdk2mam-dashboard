16425c527211f77240e054c74d7d246a5552d26a
eduardopelitti <eduardopelitti@gmail.com>
2019-04-27 18:30:02 -0300

    Add Home Component

--- frontend/src/layout/utils.js
+++ frontend/src/layout/utils.js
@@ -1,15 +1,42 @@
-const axios = require('axios')
-
 export function getLast(number) {
-    return new Promise(async (res, rej) => {
-        try {
-            const respDB = await axios.get(
-                `http://localhost:8081/api/getLast/${number}`
-            )   
-            const data = respDB.data            
-            res({data})
-        } catch(e) {
-            rej(e)
-        }
-    })
+  return new Promise(async (res, rej) => {
+    try {
+      const data = [
+        {
+          xdk2mam: [
+            {
+              sensorType: 'Environmental',
+              data: [
+                { name: 'Pressure', value: '100809' },
+                { name: 'Temperature', value: '34280' },
+                { name: 'Humidity', value: '36' },
+              ],
+            },
+            {
+              sensorType: 'Accelerometer',
+              data: [{ name: 'x', value: '-119' }, { name: 'y', value: '-7' }, { name: 'z', value: '1000' }],
+            },
+            {
+              sensorType: 'Gyroscope',
+              data: [{ name: 'x', value: '-5249' }, { name: 'y', value: '-3906' }, { name: 'z', value: '-3051' }],
+            },
+            {
+              sensorType: 'Inertial',
+              data: [{ name: 'x', value: '-366' }, { name: 'y', value: '366' }, { name: 'z', value: '-366' }],
+            },
+            { sensorType: 'Light', data: [{ name: 'milliLux', value: '54360' }] },
+            {
+              sensorType: 'Magnetometer',
+              data: [{ name: 'x', value: '46' }, { name: 'y', value: '-42' }, { name: 'z', value: '-3' }],
+            },
+          ],
+          device: 'XDKDevice1',
+          timestamp: '159',
+        },
+      ]
+      res(data)
+    } catch (e) {
+      rej(e)
+    }
+  })
 }
