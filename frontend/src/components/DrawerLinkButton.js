152eff533a6144e1c76bb2d13b94b3c5d5852dc3
Andrés Espagnolo <andyespagnolo@gmail.com>
2019-04-28 15:19:32 -0300

    Setup routes

--- frontend/src/components/DrawerLinkButton.js
+++ frontend/src/components/DrawerLinkButton.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import { ListItemText, ListItem, ListItemIcon } from '@material-ui/core'
+import { Link } from 'react-router-dom'
+import { ListItemText, ListItem } from '@material-ui/core'
 
 /**
  * DrawerLinkButton
@@ -8,15 +9,14 @@ import { ListItemText, ListItem, ListItemIcon } from '@material-ui/core'
 
 class DrawerLinkButton extends PureComponent {
   render() {
-    const { href, icon, text } = this.props
+    const { text, to } = this.props
 
     return (
-      <a prefetch href={href}>
-        <ListItem button>
-          <ListItemIcon>{icon}</ListItemIcon>
-          <ListItemText primary={text} />
+      <li>
+        <ListItem button component={Link} to={to}>
+          <ListItemText inset primary={text} />
         </ListItem>
-      </a>
+      </li>
     )
   }
 }
@@ -26,9 +26,8 @@ class DrawerLinkButton extends PureComponent {
  */
 
 DrawerLinkButton.propTypes = {
-  icon: PropTypes.node.isRequired,
-  href: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
 }
 
 /**
