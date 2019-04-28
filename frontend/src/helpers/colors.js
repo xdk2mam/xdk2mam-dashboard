a646c544b1a15dc9456f1ea86a122852d0e83f51
Eduardo Pelitti <eduardo@underscope.io>
2019-04-28 11:53:31 -0300

    Modify color palette

--- frontend/src/helpers/colors.js
+++ frontend/src/helpers/colors.js
@@ -1,22 +1,21 @@
 const Colors = Object.freeze({
-  //Monochromatic
-  LIGHT_BLUE: '#9ED2FF', 
-  PALE_LIGHT_BLUE: '#7EA8CC',
-  DARK_BLUE: '295780',
-  BLUE: '#52AEFF',
-  PALE_DARK_BLUE: '#4F6980',
-  WHITE: '#FFFFFF',  
-  //Analog
-  LIGHT_GREEN: '#91FFF8',
-  STRONG_GREEN: '#84D4E8',
-  TURQUOISE: '#9ED2FF',
-  PALE_BLUE: '#849FE8',
-  VIOLET_BLUE: '#9192FF',
-  //Basic
+  BASE: '#221778',
+  // Analog
+  BLUE: '#14268F',
+  OFF_BLUE: '#134285',
+  VIOLET: '#44148F',
+  PURPLE: '#5E1385',
+  // Basic
   BLACK: '#000000',
   WHITE: '#FFFFFF',
-  //Tones
-  DARKEST_BLUE: '#283440'
+  // Tones
+  DARKEST_BLUE: '#100B38',
+  BLEU: '#3825C4',
+  // Complementary
+  COMP_YELLOW: '#C4A900',
+  COMP_BROWN: '#786A17',
+  COMP_BLUE: '#2713C4',
+  COMP_PURPLE: '#4939C4',
 })
 
 /*
