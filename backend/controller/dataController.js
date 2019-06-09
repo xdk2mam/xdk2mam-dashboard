b0f4c9a9c560609b2d7f2f138594aca88566db17
Eduardo Pelitti <eduardo@underscope.io>
2019-06-09 18:39:17 -0300

    Working config and minor changes for device integration

--- backend/controller/dataController.js
+++ backend/controller/dataController.js
@@ -1,38 +1,40 @@
+var dataService = require('./../service/dataService.js')
+var _ = require('underscore-node')
 
-var dataService = require('./../service/dataService.js');
-var _ = require('underscore-node');
+var DataController = function() {
+  this.getData = function(req, res) {
+    dataService
+      .getData(req.query.timestamp)
+      .then(function(data) {
+        res.status(200).json(data)
+      })
+      .catch(function(err) {
+        res.status(400).send(err)
+      })
+  }
 
+  this.getLast = function(req, res) {
+    dataService
+      .getLast(req.params.number)
+      .then(function(data) {
+        res.status(200).json(data)
+      })
+      .catch(function(err) {
+        res.status(400).send(err)
+      })
+  }
 
-var DataController = function () {
+  this.putData = function(req, res) {
+    var dataSensors = req.body
+    dataService
+      .putData(dataSensors)
+      .then(function(data) {
+        res.status(200).json(data)
+      })
+      .catch(function(err) {
+        res.status(400).send(err)
+      })
+  }
+}
 
-    this.getData = function (req, res) {
-        dataService.getData(req.query.timestamp)
-            .then(function (data) {
-                res.status(200).json(data);
-            }).catch(function (err) {
-                res.status(400).send(err);
-            });
-    };
-
-    this.getLast = function (req, res) {
-        dataService.getLast(req.params.number)
-            .then(function (data) {
-                res.status(200).json(data);
-            }).catch(function (err) {
-                res.status(400).send(err);
-            });
-    }
-
-    this.putData = function (req, res) {
-        var dataSensors = req.body;
-        dataService.putData(dataSensors)
-            .then(function (data) {
-                res.status(200).json(data);
-            }).catch(function (err) {
-                res.status(400).send(err);
-            });
-    };
-
-};
-
-module.exports = new DataController();
+module.exports = new DataController()
--- backend/service/dataService.js
+++ backend/service/dataService.js
@@ -1,50 +1,50 @@
+var dataRepository = require('../repository/dataRepository.js')
+var q = require('q')
+var _ = require('underscore-node')
 
-var dataRepository = require('../repository/dataRepository.js');
-var q = require('q');
-var _ = require('underscore-node');
+var DataService = function() {
+  this.getData = function(timestamp) {
+    var deferred = q.defer()
+    dataRepository
+      .getData(timestamp)
+      .then(function(data) {
+        deferred.resolve(data)
+      })
+      .catch(function(err) {
+        deferred.reject({ errorMessage: err })
+      })
+    return deferred.promise
+  }
 
+  this.getLast = function(last) {
+    var deferred = q.defer()
 
+    dataRepository
+      .getLast(last)
+      .then(function(data) {
+        deferred.resolve(data)
+      })
+      .catch(function(err) {
+        deferred.reject({ errorMessage: err })
+        console.log('Error in dataService:', err)
+      })
+    return deferred.promise
+  }
 
-var DataService = function () {
+  this.putData = function(dataSensors) {
+    var deferred = q.defer()
+    dataRepository
+      .putData(dataSensors)
+      .then(function(data) {
+        deferred.resolve(data)
+        console.log('Inputting Data: ', data)
+      })
+      .catch(function(err) {
+        deferred.reject({ errorMessage: err })
+        console.log('Inputting Data: ', err)
+      })
+    return deferred.promise
+  }
+}
 
-    this.getData = function (timestamp) {
-        var deferred = q.defer();
-        dataRepository.getData(timestamp)
-            .then(function (data) {
-                deferred.resolve(data);
-            })
-            .catch(function (err) {
-                deferred.reject({ errorMessage: err });
-            });
-        return deferred.promise;
-    };
-
-     this.getLast= function (last) {
-        var deferred = q.defer();
-        dataRepository.getLast(last)
-            .then(function (data) {
-                deferred.resolve(data);
-            })
-            .catch(function (err) {
-                deferred.reject({ errorMessage: err });
-            });
-        return deferred.promise;
-    };
-
-    this.putData = function (dataSensors) {
-        var deferred = q.defer();
-        dataRepository.putData(dataSensors)
-            .then(function (data) {
-                deferred.resolve(data);
-            })
-            .catch(function (err) {
-                deferred.reject({ errorMessage: err });
-            });
-        return deferred.promise;
-    };
-
-
-};
-
-module.exports = new DataService();
-
+module.exports = new DataService()
