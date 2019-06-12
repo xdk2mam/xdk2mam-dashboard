f79c66140b0f281657f5eba168cab4b962beb072
Andrés Espagnolo <andyespagnolo@gmail.com>
2019-06-12 20:00:45 -0300

    Add validation to setting form

--- frontend/src/helpers/validation.js
+++ frontend/src/helpers/validation.js
@@ -6,3 +6,11 @@ export const CreateDatasetSchema = yup.object().shape({
   deviceName: yup.string().required('Device name is required'),
   endDate: yup.date().required('End date is required'),
 })
+
+export const SettingEmailSchema = yup.object().shape({
+  email: yup.string().email('Please enter a valid email'),
+})
+
+export const SettingFullNodeIotaUrlSchema = yup.object().shape({
+  fullNodeIotaUrl: yup.string().url('Please enter a valid url'),
+})
